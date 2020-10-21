b7d344acdf27560ce3c6a69e3f0b937f7af5c48e
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-21 14:24:37 -0700

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
-end
+    i = 0
+    collectionArray = []
+    while i < collection.length
+    collectionArray << yield(collection[i])
+      i = i + 1  
+    end
+    if collectionArray.include?(false)
+        false
+      else
+        true
+    end
+end 
+  
